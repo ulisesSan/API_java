6c30708478f6e88b845615eabdd8be96ca5e0989
Ulises Caceres <uliseo_95@hotmail.com>
2022-04-30 23:02:03 -0700

    Created proyect

---
+++ src/test/java/com/buses/rastearBuses/RastearBusesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buses.rastearBuses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RastearBusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
